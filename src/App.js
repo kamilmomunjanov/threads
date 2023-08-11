0752bcacb6b2aff8cfebb94dc25cb5fffb37703f
Kamil <kamilmomunjanov@gmail.com>
2023-08-11 18:45:08 +0600

    forgot password layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom";
 import Login from "./Components/Sign-In/Login/Login";
+import ForgotPassword from "./Components/Sign-In/ForgotPassword/ForgotPassword";
 
 function App() {
   return (
     <div className="App">
         <Routes>
             <Route path="/" element={<Login/>}/>
+            <Route path="/login/password" element={<ForgotPassword/>}/>
         </Routes>
     </div>
   );
---
+++ src/Components/Sign-In/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from "./ForgotPassword.module.css";
+import Logo from "../../images/svg/Pattern.svg";
+
+const ForgotPassword = () => {
+    return (
+        <div className={styles.login}>
+            <img src={Logo} alt="Logotype"/>
+            <div>
+                <h1 className={styles.title}>Forgot password ?</h1>
+                <p className={styles.subtitle}>Enter your email address to reset password</p>
+                <form className={styles.form}>
+                    <input className={styles.email} type="text" placeholder="Your email "/>
+                    <button className={styles.btn} type="submit">Continue</button>
+                </form>
+            </div>
+
+        </div>
+    );
+};
+
+export default ForgotPassword;
