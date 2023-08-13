6157dcf0d6dc60d2dfbc41ae36aceac6233d0a8f
Kamil <kamilmomunjanov@gmail.com>
2023-08-14 00:32:15 +0600

    profile page with threads

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -12,32 +12,34 @@ import content from "../images/svg/profile/content.svg";
 const Layout = () => {
     return (
         <div className={styles.tabBar}>
-            <div className={styles.tab__logo}>
-                <img src={logo} alt="Threads-Logo"/>
-            </div>
-            <div className={styles.tab__asideText}>
-                <img src={home} alt="Home"/>
-                <Link to="#" className={styles.tab__text}>Home</Link>
-            </div>
-            <div className={styles.tab__asideText}>
-                <img src={search} alt="Search"/>
-                <Link to="#" className={styles.tab__text}>Search</Link>
-            </div>
-            <div className={styles.tab__asideText}>
-                <img src={write} alt="Write a threads"/>
-                <Link to="#" className={styles.tab__text}>Write a threads</Link>
-            </div>
-            <div className={styles.tab__asideText}>
-                <img src={like} alt="Activity"/>
-                <Link to="#" className={styles.tab__text}>Activity</Link>
-            </div>
-            <div className={styles.tab__asideText}>
-                <img src={profile} alt="Activity"/>
-                <Link to="/profile" className={styles.tab__text}>Profile</Link>
-            </div>
-            <div className={`${styles.tab__asideText} ${styles.content}`}>
-                <img src={content} alt="More"/>
-                <p className={styles.tab__text}>More</p>
+            <div className={styles.positionFixed}>
+                <div className={styles.tab__logo}>
+                    <img src={logo} alt="Threads-Logo"/>
+                </div>
+                <div className={styles.tab__asideText}>
+                    <img src={home} alt="Home"/>
+                    <Link to="#" className={styles.tab__text}>Home</Link>
+                </div>
+                <div className={styles.tab__asideText}>
+                    <img src={search} alt="Search"/>
+                    <Link to="#" className={styles.tab__text}>Search</Link>
+                </div>
+                <div className={styles.tab__asideText}>
+                    <img src={write} alt="Write a threads"/>
+                    <Link to="#" className={styles.tab__text}>Write a threads</Link>
+                </div>
+                <div className={styles.tab__asideText}>
+                    <img src={like} alt="Activity"/>
+                    <Link to="#" className={styles.tab__text}>Activity</Link>
+                </div>
+                <div className={styles.tab__asideText}>
+                    <img src={profile} alt="Activity"/>
+                    <Link to="/profile" className={styles.tab__text}>Profile</Link>
+                </div>
+                <div className={`${styles.tab__asideText} ${styles.content}`}>
+                    <img src={content} alt="More"/>
+                    <p className={styles.tab__text}>More</p>
+                </div>
             </div>
         </div>
     );
--- src/Components/Profile/ProfilePage/ProfilePage.jsx
+++ src/Components/Profile/ProfilePage/ProfilePage.jsx
@@ -3,6 +3,14 @@ import styles from "./ProfilePage.module.css";
 import Layout from "../../Layout/Layout";
 import next from "../../images/svg/profile/Next.svg";
 import avatar from "../../images/png/profilePhoto.png";
+import repost from "../../images/svg/profile/repost.svg";
+import man from "../../images/png/Avatar.png";
+import woman from "../../images/png/AvatarGirl.png";
+import like from "../../images/svg/icons/Like.svg";
+import comment from "../../images/svg/icons/Comment.svg";
+import repostIcon from "../../images/svg/icons/Repost.svg";
+import send from "../../images/svg/icons/Send.svg";
+import dot from "../../images/svg/icons/Text.svg";
 
 
 const ProfilePage = () => {
@@ -29,7 +37,66 @@ const ProfilePage = () => {
                     <button className={styles.btn}>Share profile</button>
                 </div>
                 <p className={styles.text}>Threads</p>
-                <p className={styles.textNotPost}>You haven’t posted any threads yet.</p>
+                {/*<p className={styles.textNotPost}>You haven’t posted any threads yet.</p>*/}
+                <div className={styles.repost}>
+                    <img src={repost} alt="Repost-icon"/>
+                    <span className={styles.span}>You reposted</span>
+                </div>
+                <div className={styles.threads}>
+                    <img className={styles.avatar} src={man} alt="User-man"/>
+                    <div className={styles.body}>
+                        <div>
+                            <h3 className={styles.bodyTitle}>mountain_mama</h3>
+                            <p className={styles.bodySubtitle}>Innovation sets leaders apart from followers.</p>
+                            <div className={styles.icons}>
+                                <img className={styles.icon} src={like} alt="Like"/>
+                                <img className={styles.icon} src={comment} alt="Comment"/>
+                                <img className={styles.icon} src={repostIcon} alt="Repost"/>
+                                <img className={styles.icon} src={send} alt="Send"/>
+                            </div>
+                            <p className={styles.bodyText}>87 likes</p>
+                        </div>
+                        <p className={styles.timeAgo}>3m</p>
+                    </div>
+                </div>
+                <div className={styles.threads}>
+                    <img className={styles.avatar} src={woman} alt="User-woman"/>
+                    <div className={styles.body}>
+                        <div>
+                            <h3 className={styles.bodyTitle}>jhon_idon</h3>
+                            <p className={styles.bodySubtitle}>When I look at you, I see someone who’s working hard</p>
+                            <div className={styles.icons}>
+                                <img className={styles.icon} src={like} alt="Like"/>
+                                <img className={styles.icon} src={comment} alt="Comment"/>
+                                <img className={styles.icon} src={repostIcon} alt="Repost"/>
+                                <img className={styles.icon} src={send} alt="Send"/>
+                            </div>
+                            <div className={styles.positionDot}>
+                                <p className={styles.bodyText}>2 replies</p>
+                                <span className={styles.dot}>.</span>
+                                <p className={styles.bodyText}>941 likes</p>
+                            </div>
+                        </div>
+                        <p className={styles.timeAgo}>2h</p>
+                    </div>
+                </div>
+                <div className={styles.threads}>
+                    <img className={styles.avatar} src={man} alt="User-man"/>
+                    <div className={styles.body}>
+                        <div>
+                            <h3 className={styles.bodyTitle}>mountain_mama</h3>
+                            <p className={styles.bodySubtitle}>Innovation sets leaders apart from followers.</p>
+                            <div className={styles.icons}>
+                                <img className={styles.icon} src={like} alt="Like"/>
+                                <img className={styles.icon} src={comment} alt="Comment"/>
+                                <img className={styles.icon} src={repostIcon} alt="Repost"/>
+                                <img className={styles.icon} src={send} alt="Send"/>
+                            </div>
+                            <p className={styles.bodyText}>87 likes</p>
+                        </div>
+                        <p className={styles.timeAgo}>3m</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
