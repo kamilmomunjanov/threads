e89d17f5203df5eb1ce137edb2c5776f730387cf
Kamil <kamilmomunjanov@gmail.com>
2023-08-13 23:13:19 +0600

    profile page without threads

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import styles from "./Layout.module.css";
+import logo from "../images/svg/profile/Vector.svg";
+import home from "../images/svg/profile/Home.svg";
+import {Link} from "react-router-dom";
+import search from "../images/svg/profile/Search.svg";
+import write from "../images/svg/profile/Write.svg";
+import like from "../images/svg/profile/like.svg";
+import profile from "../images/svg/profile/profile.svg";
+import content from "../images/svg/profile/content.svg";
 
 const Layout = () => {
     return (
-        <div>
-
+        <div className={styles.tabBar}>
+            <div className={styles.tab__logo}>
+                <img src={logo} alt="Threads-Logo"/>
+            </div>
+            <div className={styles.tab__asideText}>
+                <img src={home} alt="Home"/>
+                <Link to="#" className={styles.tab__text}>Home</Link>
+            </div>
+            <div className={styles.tab__asideText}>
+                <img src={search} alt="Search"/>
+                <Link to="#" className={styles.tab__text}>Search</Link>
+            </div>
+            <div className={styles.tab__asideText}>
+                <img src={write} alt="Write a threads"/>
+                <Link to="#" className={styles.tab__text}>Write a threads</Link>
+            </div>
+            <div className={styles.tab__asideText}>
+                <img src={like} alt="Activity"/>
+                <Link to="#" className={styles.tab__text}>Activity</Link>
+            </div>
+            <div className={styles.tab__asideText}>
+                <img src={profile} alt="Activity"/>
+                <Link to="/profile" className={styles.tab__text}>Profile</Link>
+            </div>
+            <div className={`${styles.tab__asideText} ${styles.content}`}>
+                <img src={content} alt="More"/>
+                <p className={styles.tab__text}>More</p>
+            </div>
         </div>
     );
 };
--- src/Components/Profile/ProfilePage/ProfilePage.jsx
+++ src/Components/Profile/ProfilePage/ProfilePage.jsx
@@ -1,49 +1,35 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import styles from "./ProfilePage.module.css";
-import logo from "../../images/svg/profile/Vector.svg";
-import home from "../../images/svg/profile/Home.svg";
-import like from "../../images/svg/profile/like.svg";
-import search from "../../images/svg/profile/Search.svg";
-import write from "../../images/svg/profile/Write.svg";
-import profile from "../../images/svg/profile/profile.svg";
-import content from "../../images/svg/profile/content.svg";
+import Layout from "../../Layout/Layout";
+import next from "../../images/svg/profile/Next.svg";
+import avatar from "../../images/png/profilePhoto.png";
+
 
 const ProfilePage = () => {
     return (
         <div className={styles.profilePage}>
-            <div className={styles.tabBar}>
-                <div className={styles.tab__logo}>
-                    <img src={logo} alt="Threads-Logo"/>
-                </div>
-                <div className={styles.tab__asideText}>
-                    <img src={home} alt="Home"/>
-                    <Link to="#" className={styles.tab__text}>Home</Link>
-                </div>
-                <div className={styles.tab__asideText}>
-                    <img src={search} alt="Search"/>
-                    <Link to="#" className={styles.tab__text}>Search</Link>
-                </div>
-                <div className={styles.tab__asideText}>
-                    <img src={write} alt="Write a threads"/>
-                    <Link to="#" className={styles.tab__text}>Write a threads</Link>
-                </div>
-                <div className={styles.tab__asideText}>
-                    <img src={like} alt="Activity"/>
-                    <Link to="#" className={styles.tab__text}>Activity</Link>
-                </div>
-                <div className={styles.tab__asideText}>
-                    <img src={profile} alt="Activity"/>
-                    <Link to="/profile" className={styles.tab__text}>Profile</Link>
-                </div>
-                <div className={`${styles.tab__asideText} ${styles.content}`}>
-                    <img src={content} alt="More"/>
-                    <p className={styles.tab__text}>More</p>
-                </div>
-            </div>
-
+            <Layout/>
             <div className={styles.loginPage__right}>
-
+                    <div className={styles.info}>
+                        <div className={styles.information}>
+                            <img src={avatar} alt="Avatar"/>
+                            <div className={styles.aboutUser}>
+                                <h2 className={styles.title}>Design lead</h2>
+                                <p className={styles.subtitle}>malevicz
+                                    <a className={styles.threadsNet} target="_blank"
+                                       href="https://www.threads.net/">threads.net</a>
+                                </p>
+                                <p className={styles.followers}>153K followers</p>
+                            </div>
+                        </div>
+                        <img className={styles.nextPage} src={next} alt="Next page"/>
+                    </div>
+                <div className={styles.buttons}>
+                    <button className={styles.btn}>Edit profile</button>
+                    <button className={styles.btn}>Share profile</button>
+                </div>
+                <p className={styles.text}>Threads</p>
+                <p className={styles.textNotPost}>You haven’t posted any threads yet.</p>
             </div>
         </div>
     );
