aeaa8222b8375a4d10e40eec77a9a5d2f4e3b4a8
Kamil <kamilmomunjanov@gmail.com>
2023-08-22 09:32:22 +0600

    aside search

---
+++ src/Components/Layout/Aside.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import "./aside.css";
+import repost from "../images/svg/modal/Repost.svg";
+import styles from "./Layout.module.css";
+import ava from "../images/svg/main/avatar.svg";
+
+const Aside = ({aside, setAside}) => {
+    return (
+        <div className={aside ? "aside active" : "aside"}>
+            <div className={aside ? "aside__content active" : "aside__content"}>
+                <h4 className="search" onClick={(e) => {
+                    e.stopPropagation()
+                    setAside(false)
+                }}>Search</h4>
+                <input className="input__search" type="text" placeholder="Search"/>
+
+                <div className={styles.suggest}>
+                    <div className={styles.user}>
+                        <img style={{cursor:"pointer"}} src={ava} alt="avatar"/>
+                        <div>
+                            <h4 className={styles.title}>mkbhd</h4>
+                            <p className={styles.subtitle}>Follows you</p>
+                            <p className="followers">50 followers</p>
+                        </div>
+                    </div>
+                    <button className={styles.btn}>Following</button>
+                </div>
+
+                <div className={styles.suggest}>
+                    <div className={styles.user}>
+                        <img style={{cursor:"pointer"}} src={ava} alt="avatar"/>
+                        <div>
+                            <h4 className={styles.title}>mkbhd</h4>
+                            <p className={styles.subtitle}>Follows you</p>
+                            <p className="followers">789 followers</p>
+                        </div>
+                    </div>
+                    <button className={styles.btn}>Follow</button>
+                </div>
+
+                <div className={styles.suggest}>
+                    <div className={styles.user}>
+                        <img style={{cursor:"pointer"}} src={ava} alt="avatar"/>
+                        <div>
+                            <h4 className={styles.title}>mkbhd</h4>
+                            <p className={styles.subtitle}>Follows you</p>
+                            <p className="followers">123 followers</p>
+                        </div>
+                    </div>
+                    <button className={styles.btn}>Follow</button>
+                </div>
+
+                <div className={styles.suggest}>
+                    <div className={styles.user}>
+                        <img style={{cursor:"pointer"}} src={ava} alt="avatar"/>
+                        <div>
+                            <h4 className={styles.title}>mkbhd</h4>
+                            <p className={styles.subtitle}>Follows you</p>
+                            <p className="followers">5.3K followers</p>
+                        </div>
+                    </div>
+                    <button className={styles.btn}>Follow</button>
+                </div>
+
+                <div className={styles.suggest}>
+                    <div className={styles.user}>
+                        <img style={{cursor:"pointer"}} src={ava} alt="avatar"/>
+                        <div>
+                            <h4 className={styles.title}>mkbhd</h4>
+                            <p className={styles.subtitle}>Follows you</p>
+                            <p className="followers">1.2K followers</p>
+                        </div>
+                    </div>
+                    <button className={styles.btn}>Following</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Aside;
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -10,39 +10,45 @@ import profile from "../images/svg/profile/profile.svg";
 import content from "../images/svg/profile/content.svg";
 import ModalPage from "../Profile/modalPage/ModalPage";
 import LayoutModal from "./LayoutModal";
+import Aside from "./Aside";
 
 const Layout = () => {
     const [openModal, setOpenModal] = useState(false)
+    const [aside, setAside] = useState(false)
 
     return (
         <div className={styles.tabBar}>
+            <Aside aside={aside} setAside={setAside}/>
             <div className={styles.positionFixed}>
                 <div className={styles.tab__logo}>
                     <img src={logo} alt="Threads-Logo"/>
                 </div>
                 <div className={styles.tab__asideText}>
-                    <img src={home} alt="Home"/>
-                    <Link to="#" className={styles.tab__text}>Home</Link>
+                    <Link to="/home"><img src={home} alt="Home"/></Link>
+                    <Link to="/home" className={aside ? `${styles.tab__text} ${styles.tab__none}` : `${styles.tab__text}`}>Home</Link>
+                </div>
+                <div onClick={e => {
+                    e.stopPropagation()
+                    setAside(true)
+                }} className={styles.tab__asideText}>
+                    <img className={aside ? ` ${styles.border}` : `` } src={search} alt="Search"/>
+                    <Link  to="#" className={aside ? `${styles.tab__text} ${styles.tab__none}` : `${styles.tab__text}`}>Search</Link>
                 </div>
                 <div className={styles.tab__asideText}>
-                    <img src={search} alt="Search"/>
-                    <Link to="#" className={styles.tab__text}>Search</Link>
+                    <Link to="/home"><img src={write} alt="Write a threads"/></Link>
+                    <Link to="/home" className={aside ? `${styles.tab__text} ${styles.tab__none}` : `${styles.tab__text}`}>Write a threads</Link>
                 </div>
                 <div className={styles.tab__asideText}>
-                    <img src={write} alt="Write a threads"/>
-                    <Link to="#" className={styles.tab__text}>Write a threads</Link>
+                    <Link to="#"><img src={like} alt="Activity"/></Link>
+                    <Link to="#" className={aside ? `${styles.tab__text} ${styles.tab__none}` : `${styles.tab__text}`}>Activity</Link>
                 </div>
                 <div className={styles.tab__asideText}>
-                    <img src={like} alt="Activity"/>
-                    <Link to="#" className={styles.tab__text}>Activity</Link>
-                </div>
-                <div className={styles.tab__asideText}>
-                    <img src={profile} alt="Activity"/>
-                    <Link to="/profile" className={styles.tab__text}>Profile</Link>
+                    <Link to="/profile"><img src={profile} alt="Activity"/></Link>
+                    <Link to="/profile" className={aside ? `${styles.tab__text} ${styles.tab__none}` : `${styles.tab__text}`}>Profile</Link>
                 </div>
                 <div className={`${styles.tab__asideText} ${styles.content}`}>
                     <img src={content} alt="More"/>
-                    <p onClick={() => setOpenModal(true)} className={styles.tab__text}>More</p>
+                    <p onClick={() => setOpenModal(true)} className={aside ? `${styles.tab__text} ${styles.tab__none}` : `${styles.tab__text}`}>More</p>
                     <LayoutModal modal={openModal} setModal={setOpenModal}/>
                 </div>
             </div>
